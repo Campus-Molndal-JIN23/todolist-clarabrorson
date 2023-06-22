f1b95a661471c994405889be14e72c671331ff70
clarabrorson <88clabro@gafe.molndal.se>
2023-06-22 14:06:37 +0200

    Update Application.java
    lagt till metodhuvud

--- TDDproject/src/main/java/org/campusmolndal/Application.java
+++ TDDproject/src/main/java/org/campusmolndal/Application.java
@@ -5,5 +5,21 @@ import java.util.Scanner;
 
 public class Application {
 
+    public void addToDo(){
+
+    }
+    public void showToDo(){
+
+    }
+
+    public void showAllToDos(){
+
+    }
+    public void updateToDo(){
+
+    }
+    public void deleteToDo(){
+
+    }
 }
 
